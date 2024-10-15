8d026f07958ab9b6b36cdee67742b513245fd65f
teju-loki2 <tejaswini.yallamraju2@gmail.com>
2024-10-15 10:30:36 +0530

    Created

---
+++ src/content_option.js
@@ -0,0 +1,116 @@
+const logotext = "TEJ";
+const meta = {
+    title: "YALLAMRAJU TEJASWINI",
+    description: "I’m Tejaswini  Full stack devloper,currently studying",
+};
+
+const introdata = {
+    title: "I’m Tejaswini",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a student with intermediate web developing skills,creating some new websites",
+    your_img_url: "https://storage.prompt-hunt.workers.dev/clf9emhen0001k1085rly0ill_1",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Digital architect, crafting intelligent solutions through code to solve complex problems",
+};
+const certifications = [{
+        title: "CSS,JAVASCRIPT AND PYTHON",
+        description: "UDEMY",
+        
+    },
+    {
+        title: "The complete intro to SQL",
+        description: "UDEMY",
+    },
+    {
+        title: "DEEP LEARNING",
+        description: "NPTEL",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Html",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 65,
+    },
+    {
+        name: "Java",
+        value: 80,
+    },
+];
+
+const services= [{
+        title: "Rebuy",
+        description: "This helps people who cannot afford expensive books as the cost of resold books is much lower.",
+    },
+    {
+        title: "Hunger Halters",
+        description: "The main aim of the project is to reduce the hunger deaths to an extent by providing interface between the people who are willing to help the needy.",
+    },
+    {
+        title: "Car Rental",
+        description: "Full stack website which helps the users to rent a car on daily,weekly or monthly basis. ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.im.ge/2023/11/08/yEtaZx.hunger.jpg",
+        description: "HUNGER HALTERS",
+        link: "#",
+    },
+
+    {
+        img: "https://i.im.ge/2023/11/08/yECIi1.rebuy.jpg",
+        description: "REBUY",
+        link: "#",
+    },
+    
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tejaswini.yallamraju2@gmail.com",
+    YOUR_PHONE: "9182763694",
+    description: " Lets Connect ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_l6gcb08",
+    YOUR_TEMPLATE_ID: "template_5nxcaiy",
+    YOUR_USER_ID: "lFIV4Ua53othFMGbF",
+};
+
+const socialprofils = {
+    github: "https://github.com/teju-loki2",
+    facebook: "https://www.facebook.com/profile.php?id=100076821739677&mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/yallamraju-tejaswini-116362233",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    certifications,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
